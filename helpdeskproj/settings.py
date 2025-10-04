7fc77ff2f85c37405e58d295cc66130d388b0f22
ILLA MANASA <118953792+IllaMANASA@users.noreply.github.com>
2025-10-04 15:00:25 +0530

    Update settings.py

--- helpdeskproj/settings.py
+++ helpdeskproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1i4t+hgx*xjl2v#z(8@r09&6i72(m9j6eo)^&8dfyv#3hliz_7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sayyadarshad.pythonanywhere.com']
 
 
 # Application definition
